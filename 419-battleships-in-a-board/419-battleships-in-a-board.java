fd9f0f3f562d9864f387420de82809a9c9898c42
Victor Nyarongi <vogonyo101@gmail.com>
2022-04-11 13:14:42 +0300

    Time: 0 ms (100.00%), Space: 42.1 MB (82.27%) - LeetHub

---
+++ 419-battleships-in-a-board/419-battleships-in-a-board.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int countBattleships(char[][] board) {
+        int numBattleships = 0;
+        for(int i=0; i< board.length;i++){
+            for(int j=0; j<board[i].length; j++){
+                if(board[i][j] == 'X'){
+                    numBattleships++;
+                    sink(board, i, j);
+                }
+            }
+        }
+        return numBattleships;
+        
+    }
+    public void sink(char[][] board, int i, int j){
+        if(i < 0 || i >= board.length || j < 0 || j >= board[i].length || board[i][j] != 'X'){
+            return;
+        }
+        board[i][j] = '.';
+        sink(board, i+1, j);
+        sink(board, i -1, j);
+        sink(board, i, j+1);
+        sink(board, i, j-1);
+    }
+}
