e5884511c6128aeebdc7bf186b4ff66a18d75a51
Victor Nyarongi <vogonyo101@gmail.com>
2022-04-21 11:03:16 +0300

    Time: 5 ms (64.83%), Space: 42.2 MB (98.24%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(candidates);
+        findCombinations(candidates, 0, target, new ArrayList<Integer>(), result);
+        return result;
+    }
+    
+    public void findCombinations(int[] candidates, int index, int target, List<Integer> current, List<List<Integer>> result){
+        // Base  Case when target = 0 result
+        if(target == 0 ){
+            result.add(new ArrayList<>(current));
+            return;
+        }
+        
+        if(target < 0){
+            return;
+        }
+        
+        for(int i =index; i < candidates.length; i++){
+            //Check for duplicates
+            if(i == index || candidates[i] != candidates[i -1]){
+                current.add(candidates[i]);
+                findCombinations(candidates, i + 1, target - candidates[i], current, result);
+                current.remove(current.size() - 1);
+                
+            }
+        }
+    }
+}
