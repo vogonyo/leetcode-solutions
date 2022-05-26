fdebf5c3276f713636700503c35c149eb5cdfd65
Victor Nyarongi <vogonyo101@gmail.com>
2022-05-26 12:21:11 +0300

    Added solution - LeetHub

--- Restrictive Candy Crush - GFG/restrictive-candy-crush.cpp
+++ Restrictive Candy Crush - GFG/restrictive-candy-crush.cpp
@@ -9,41 +9,37 @@ class Solution{
     public:
     string Reduced_String(int k,string s){
         // Your code goes here
-        //Base case if k ==1 all characters can be removed
+        int n = s.length();
         if(k == 1) return "";
         string output = "";
-        
         stack<pair<char, int>> stk;
-        
-       for(int i=0; i<s.length(); i++){
-           if(stk.empty()  == true){
+        for(int i=0; i<n; i++){
+            if(stk.empty() == true){
                 stk.push(make_pair(s[i], 1));
-           }
-           else{
-               if(s[i] == (stk.top()).first){
-                   stk.push({s[i], stk.top().second + 1});
-                   if(stk.top().second == k ){
-                       int x =k;
-                       while(x){
-                           stk.pop();
-                           x--;
-                       }
-                   }
-               }
-               else{
-                   stk.push(make_pair(s[i], 1));
-               }
-           }
-          
-       }
-       
-       while(!stk.empty()){
-           output += stk.top().first;
-           stk.pop();
-       }
-       
-       reverse(output.begin(), output.end());
-       return output;
+            }else{
+                if(s[i] == (stk.top()).first){
+                  stk.push({s[i], stk.top().second + 1});
+                  if(stk.top().second == k){
+                      int x = k;
+                      while(x){
+                          stk.pop();
+                          x--;
+                      }
+                  }
+                }
+                else{
+                    stk.push(make_pair(s[i], 1));
+                    
+                }
+            }
+        }
+        
+        while(!stk.empty()){
+            output += stk.top().first;
+            stk.pop();
+        }
+        reverse(output.begin(), output.end());
+        return output;
     }
 
 
